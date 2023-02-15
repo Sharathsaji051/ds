43f3d885d26d42ff70f2de1bb7d2aebde34d627a
Sharathsaji051 <125427826+Sharathsaji051@users.noreply.github.com>
2023-02-15 09:56:44 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter first number");
+scanf("%d",&a);
+printf("enter second number");
+scanf("%d",&b);
+c=a+b;
+printf("sum is=%d",c);
+
+}
+
